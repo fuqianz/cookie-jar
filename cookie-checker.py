559af634577ed4be71b9c2df14b5719a46f76258
fuqianz <fuqianz@node-1.gencookiedata.senfv-pg0.clemson.cloudlab.us>
2018-06-18 15:20:27 -0400

    modify cookie checker

--- cookie-checker.py
+++ cookie-checker.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-
+import sys
 import socket
 from random import randint
 from time import sleep
@@ -11,22 +11,26 @@ def send_request(cookie):
     kukki = 'user=' + cookie
     try: 
         goofus_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        goofus_socket.connect(('172.21.3.200', 80))
-        hdr_start = 'GET /admin.html HTTP/1.1\nHost: 192.168.1.7\nCookie: '
-        request = hdr_start + kukki + '\n\n'
+        goofus_socket.connect(('130.127.133.83', 80))
+        hdr_start = 'GET /admin.html HTTP/1.1\r\nHost: 192.168.1.7\r\nCookie: '
+        request = hdr_start + kukki + '\r\n\r\n'
         goofus_socket.send(request)
     except socket.error: 
         print 'Could not connect...'
     sleep(1)
 
-def pick_cookie():
-    return POSSIBLE_COOKIES[randint(0, 49)]
+def pick_cookie(start):
+    return POSSIBLE_COOKIES[randint(start, start + 4)]
 
 if __name__ == '__main__':
-    while True:  
-        current_cookie = pick_cookie()
-        for x in range(0, 10):
-            send_request(current_cookie)
-            sleep(5)
+
+    if len (sys.argv) != 2:
+        exit()   
+
+    start = int(sys.argv[1])*5
+    while True:
+        current_cookie = pick_cookie(start)
+        send_request(current_cookie)
+        sleep(1)
     print 'Finishing due to keyboard interrupt.'
 
